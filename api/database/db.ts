dfff773dde800d13619f7c4e5be44c0fa6d728de
David Kim <dkhw90@gmail.com>
2022-07-19 19:56:02 +0200

    Remove comments

--- api/database/db.ts
+++ api/database/db.ts
@@ -2,8 +2,6 @@
 import pg from 'pg';
 import 'dotenv/config';
 
-// Placeholder database - this has the basic data that I parsed playing around with bitcoin blockchain data
-// Talk with the team about creating a new database?
 const pool = new pg.Pool({
   user: process.env.user,
   host: process.env.host,
